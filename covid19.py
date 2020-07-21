7e90780bf70c0789b6b64bcaa953921e2379169d
rafaelferrarifonseca <47007326+rafaelferrarifonseca@users.noreply.github.com>
2020-07-20 21:49:47 -0300

    Add files via upload
    dataset: dataset covid19
    https://data.brasil.io/dataset/covid19/_meta/list.html
    Data de captura: 2020-07-20

---
+++ covid19.py
@@ -0,0 +1,31 @@
+
+import pandas as pd # biblioteca DataFrames
+import numpy as np # biblioteca de  calculos matemáticos
+import matplotlib.pyplot as plt #  biblioteca de criação de gráficos
+import seaborn as sns # biblioteca de gráficos mais atraentes
+
+covid = pd.read_csv("caso_full.csv",encoding='ISO-8859-1')
+#print(covid.head(10)) #impressão das 10 primeiras linhas
+
+covid = covid.dropna(subset=['city'], axis=0) #Apago registros de cidade em branco
+
+#Analisar por estado novos casos
+casos_covid = {}
+nomes_estado = covid['state'].unique() #Nome de estado  que aparece no arquivo
+
+
+for i in nomes_estado:
+    i_estado = covid[covid['state'] == i]  # Série criada somente onde o nome do atual estado aparece
+    total_de_casos = i_estado['new_confirmed'].sum()  # Soma de todas os novos casos do estado
+    casos_covid[i] = total_de_casos  # Criando chave e valor no dicionário, convertendo os totais para Real
+    total_estado = pd.DataFrame.from_dict(casos_covid, orient='index')  # Criando DataFrame com base no dicionário
+    total_estado.columns = ['TOTAL_CASOS']  # Renomeando a coluna com valor
+
+
+total_estado = total_estado.sort_values('TOTAL_CASOS', ascending=False)  # Ordeno valores do DF
+total_estado.head(25).plot(kind='barh', figsize=(10, 5), color='green', legend=False, grid=True, alpha=0.7)  # 
+plt.show()
+
+
+
+
