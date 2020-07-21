d9766d546ef2f540935cb9d1a872c90d704391fe
rafaelferrarifonseca <47007326+rafaelferrarifonseca@users.noreply.github.com>
2020-07-20 22:05:43 -0300

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -17,7 +17,7 @@ nomes_estado = covid['state'].unique() #Nome de estado  que aparece no arquivo
 for i in nomes_estado:
     i_estado = covid[covid['state'] == i]  # Série criada somente onde o nome do atual estado aparece
     total_de_casos = i_estado['new_confirmed'].sum()  # Soma de todas os novos casos do estado
-    casos_covid[i] = total_de_casos  # Criando chave e valor no dicionário, convertendo os totais para Real
+    casos_covid[i] = total_de_casos  # Criando chave e valor no dicionário
     total_estado = pd.DataFrame.from_dict(casos_covid, orient='index')  # Criando DataFrame com base no dicionário
     total_estado.columns = ['TOTAL_CASOS']  # Renomeando a coluna com valor
 
